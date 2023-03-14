df0da2875f9ab4614dba87d1f7de7fa3aacd9913
Manuel <info@tagirijus.de>
2023-03-14 20:27:45 +0100

    Added TagiShortcuts connection

--- Template/task_sidebar/tagiaddspenttime_button.php
+++ Template/task_sidebar/tagiaddspenttime_button.php
@@ -1,5 +1,17 @@
+<?php
+
+$html = '<i class="fa fa-clock-o fa-fw js-modal-small" aria-hidden="true"></i>' . t('Add spent time');
+$href = $this->helper->url->href(
+    'TagiAddSpentTimeController',
+    'enter',
+    ['plugin' => 'TagiAddSpentTime', 'task_id' => $task['id']]
+);
+$a_element = '<a href="' . $href . '" class="js-modal-small" id="tagiAddSpentTimeMenu" data-addUrl="' . $href . '">' . $html . '</a>';
+
+?>
+
 <?php if ($task['is_active'] == 1): ?>
 <li>
-<?= $this->modal->small('clock-o', t('Add spent time'), 'TagiAddSpentTimeController', 'enter', array('plugin' => 'TagiAddSpentTime', 'task_id' => $task['id'])) ?>
+    <?php echo $a_element; ?>
 </li>
 <?php endif ?>
--- changelog.md
+++ changelog.md
@@ -1,6 +1,14 @@
 # Changelog
 
 
+## v1.1
+
+### What's Changed
+
+_(most recent changes are listed on top):_
+- Added [TagiShortcuts](https://github.com/Tagirijus/kanboard-TagiShortcuts) connection, basically
+
+
 ## v1.0
 
 ### What's Changed
